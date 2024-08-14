799b51823f7b7fc5b2f5437946cb1c35d8691bb5
Simon Müller <118167574+Appli4Ever@users.noreply.github.com>
2024-08-14 17:26:59 +0200

    Auth State Prerendering

--- Blazor_8_Prerender/Components/Account/PersistingServerAuthenticationStateProvider.cs
+++ Blazor_8_Prerender/Components/Account/PersistingServerAuthenticationStateProvider.cs
@@ -15,6 +15,7 @@ namespace Blazor_8_Prerender.Components.Account
     internal sealed class PersistingServerAuthenticationStateProvider : ServerAuthenticationStateProvider, IDisposable
     {
         private readonly PersistentComponentState state;
+        private readonly IHttpContextAccessor _contextAccessor;
         private readonly IdentityOptions options;
 
         private readonly PersistingComponentStateSubscription subscription;
@@ -23,22 +24,40 @@ namespace Blazor_8_Prerender.Components.Account
 
         public PersistingServerAuthenticationStateProvider(
             PersistentComponentState persistentComponentState,
-            IOptions<IdentityOptions> optionsAccessor)
+            IOptions<IdentityOptions> optionsAccessor,
+            IHttpContextAccessor contextAccessor)
         {
             state = persistentComponentState;
+            _contextAccessor = contextAccessor;
             options = optionsAccessor.Value;
 
             AuthenticationStateChanged += OnAuthenticationStateChanged;
             subscription = state.RegisterOnPersisting(OnPersistingAsync, RenderMode.InteractiveWebAssembly);
         }
 
+        /// <summary>
+        /// Wird nur ausgelöst während des Prerendering,
+        /// wenn der Authentication State mit<CascadingAuthenticationState>
+        /// angegeben wird.
+        /// </summary>
+        /// <returns></returns>
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            /*
-             * Wird nur ausgelöst während des Prerendering,
-             * wenn der Authentication State mit <CascadingAuthenticationState>
-             * angegeben wird.
-            */
+            if (this._contextAccessor.HttpContext.Request.Path.StartsWithSegments("/auth"))
+            {
+                /*
+                 * Hier kann Logik eingebaut werden
+                 * die den Auth State des Benutzers während Prerendering
+                 * ändert. Z.B. Kann dies von Custom Cookies abhängen oder
+                 * von einer bestimmten Route. Mit dem IHttpConextAccessor kann
+                 * der HttpContext über den Konstruktor eingefügt werden um so
+                 * Zugriff auf Cookies oder Routen zu erhalten.
+                */
+
+                // Für Test Zwecke, kann ein nicht Authorisierter Benutzer zurückgegeben werden.
+                ////return Task.FromResult(new AuthenticationState(new ClaimsPrincipal()));
+            }
+
             return base.GetAuthenticationStateAsync();
         }
 
